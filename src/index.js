8b399ac00ec52af4b27fe2eff14e87d832ee58a6
Scott Hutley <scotthutley1@comcast.net>
2021-09-10 14:26:48 -0700

    Add AUD, EUR, MXN selectors and if/else returns

--- src/index.js
+++ src/index.js
@@ -9,10 +9,19 @@ function getElements(response) {
   if (country === "USD") {
     let USDAmount = $('#dollarAmount').val() * response.conversion_rates.USD;
     $('.showMoney').html(`This exchange equates to $${USDAmount} dollars, which is exactly what you already have Charlie!`);
-  } else if (country === "AED") {
+  }  else if (country === "AED") {
     let AEDAmount = $('#dollarAmount').val() * response.conversion_rates.AED;
     $('.showMoney').html(`This exchange equates to ${AEDAmount} dirhams.`);
-  } else {
+  }  else if (country === "AUD") {
+    let AUDAmount = $('#dollarAmount').val() * response.conversion_rates.AUD;
+    $('.showMoney').html(`This exchange equates to ${AUDAmount} Australian dollars.`);
+  }  else if (country === "EUR") {
+    let EURAmount = $('#dollarAmount').val() * response.conversion_rates.EUR;
+    $('.showMoney').html(`This exchange equates to ${EURAmount} euros.`);
+  }  else if (country === "MXN") {
+    let MXNAmount = $('#dollarAmount').val() * response.conversion_rates.MXN;
+    $('.showMoney').html(`This exchange equates to ${MXNAmount} pesos.`);
+  }  else {
   $('.showErrors').text(`There was an error: ${response.message}`);
   }
 }
