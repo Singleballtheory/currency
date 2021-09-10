cf2dd644c8e79d697dd24176a057956917abd8a4
Scott Hutley <scotthutley1@comcast.net>
2021-09-10 11:51:33 -0700

    WIP Service linked in doc.ready func, still no output

--- src/index.js
+++ src/index.js
@@ -4,31 +4,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Service from './service.js';
 
+function getElements(response) {
+  if (response.name) {
+    $('.showMoney').html(`The exchange of ${response.name} is equal to ${response.conversion_rates.USD}`);
+  }  else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
 
 $(document).ready(function() {
   $('#exchangeMoney').click(function() {
-    const cash = $('#showMoney').val();
-    $('#showMoney').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
+    Service.getMoney()
+      .then(function(response) {
         getElements(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-   function getElements(response) {
-     if (response.name) {
-       $('.showMoney').html(`The exchange of ${response.name} is equal to ${response.conversion_rates.USD}`);
-     }  else {
-      $('.showErrors').text(`There was an error: ${response.message}`);
-     }
-    }
+      }).catch(function(){
+        ('.showErrors').text('This is an error');
+      });
+    // const cash = $('#showMoney').val();
+    // $('#showMoney').val("");
   });
 });
+
+// let request = new XMLHttpRequest();
+// const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+
+// request.onreadystatechange = function() {
+//   if (this.readyState === 4 && this.status === 200) {
+//     const response = JSON.parse(this.responseText);
+//     getElements(response);
+//   }
+// };
+
+// request.open("GET", url, true);
+// request.send();
